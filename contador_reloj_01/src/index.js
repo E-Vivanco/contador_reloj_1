57f0e48e1d9c7fb0417d30d6fc614235c635b819
E-Vivanco <vivanco.elsa@gmail.com>
2022-12-16 03:21:08 +0000

    1° commit reloj_contador

---
+++ contador_reloj_01/src/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import PropTypes from 'prop-types'
+//import App from './App'
+import './styles.css'
+
+function Contador_R(props){
+    return(
+    
+        <div className='Contador_Reloj container w-50'>
+            <div className='calendario'>
+             <i class="fa-solid fa-clock fa-1x"></i>
+             </div>
+                <div className='cuatro'>{props.digitocuatro % 10}</div>
+                <div className='tres'>{props.digitotres % 10}</div>
+                <div className='dos'>{props.digitodos % 10}</div>
+                <div className='uno'>{props.digitouno % 10}</div>
+            
+        </div>)
+}
+Contador_R.propTypes={
+    digitocuatro: PropTypes.number,
+    digitotres:PropTypes.number,
+    digitodos:PropTypes.number,
+    digitouno:PropTypes.number
+}
+let counter=0
+setInterval(function(){
+    const cuatro = Math.floor(counter/1000)
+    const tres = Math.floor(counter/100)
+    const dos = Math.floor(counter/10)
+    const uno = Math.floor(counter/1)
+    console.log(cuatro,tres,dos,uno)
+    counter++;
+
+ReactDOM.createRoot(document.querySelector('#root')).render(<Contador_R digitouno={uno} digitodos={dos} digitotres={tres} digitocuatro={cuatro} />)
+},1000)
